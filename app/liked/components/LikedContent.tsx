a5ae42087a7e520330a0be0ccb208fa80ddd49ed
newways <reactwebdew@gmail.com>
2023-06-27 11:17:52 +0300

    like songs

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,25 +1,36 @@
 'use client'
 
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { Song } from '@/types'
+import { useRouter } from 'next/navigation'
+import { useUser } from '@/hooks/useUser'
 import MediaItem from '@/components/MediaItem'
 import LikeButton from '@/components/LikeButton'
 
-interface SearchContentProps {
+interface LikedContentProps {
   songs: Song[]
 }
 
-const SearchContent: FC<SearchContentProps> = ({ songs }) => {
+const LikedContent: FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter()
+  const { isLoading, user } = useUser()
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/')
+    }
+  }, [isLoading, user, router])
+
   if (songs.length === 0) {
     return (
       <div className='flex flex-col gap-y-2 w-full px-6 text-neutral-400'>
-        No songs found.
+        No liked songs.
       </div>
     )
   }
 
   return (
-    <div className='flex flex-col gap-y-2 w-full px-6'>
+    <div className='flex flex-col gap-y-2 w-full p-6'>
       {songs.map((song) => (
         <div key={song.id} className='flex items-center gap-x-4 w-full'>
           <div className='flex-1'>
@@ -32,4 +43,4 @@ const SearchContent: FC<SearchContentProps> = ({ songs }) => {
   )
 }
 
-export default SearchContent
+export default LikedContent
