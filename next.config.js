245f46f0e033e52b334a6a3984d59c23b41cc81c
newways <reactwebdew@gmail.com>
2023-06-27 10:33:17 +0300

    fetch songs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['dernmcjzufxtecdpulgv.supabase.co'],
+  },
+}
 
 module.exports = nextConfig
