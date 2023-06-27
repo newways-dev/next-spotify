9ba61e067c2c6844138341b67b7b2d75d5ec59e5
newways <reactwebdew@gmail.com>
2023-06-27 12:42:23 +0300

    sound player

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
 import getSongsByUserId from '@/actions/getSongsByUserId'
+import Player from '@/components/Player'
 
 const figtree = Figtree({ subsets: ['latin'] })
 
@@ -31,6 +32,7 @@ export default async function RootLayout({
           <UserProvider>
             <ModalProvider />
             <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
--- components/Library.tsx
+++ components/Library.tsx
@@ -8,6 +8,7 @@ import useUploadModal from '@/hooks/useUploadModal'
 import { Song } from '@/types'
 import { FC } from 'react'
 import MediaItem from './MediaItem'
+import useOnPlay from '@/hooks/useOnPlay'
 
 interface LibraryProps {
   songs: Song[]
@@ -17,6 +18,8 @@ const Library: FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal()
   const uploadModal = useUploadModal()
   const { user } = useUser()
+
+  const onPlay = useOnPlay(songs)
 
   const onClick = () => {
     if (!user) {
@@ -43,7 +46,11 @@ const Library: FC<LibraryProps> = ({ songs }) => {
       </div>
       <div className='flex flex-col gap-y-2 mt-4 px-3'>
         {songs.map((song) => (
-          <MediaItem key={song.id} data={song} onClick={() => {}} />
+          <MediaItem
+            key={song.id}
+            data={song}
+            onClick={(id: string) => onPlay(id)}
+          />
         ))}
       </div>
     </div>
